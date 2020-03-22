8020c4ca085c40e38a66b41a0e1472239a009468
dkozakwsei <dkozakwsei@gmail.com>
2020-03-22 17:38:44 +0100

    waluty

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 
 using namespace std;
-int wiek;
-int main()
-{
-    cout << "Ile masz lat: ";
-    cin >> wiek;
-    if (wiek<18){
-        cout<<"Jestes niepelnoletni i nie mozesz kandydowac";
+float funty_na_pln(float funty){
+    return funty*4.83;
+}
+float pln_na_funty(float pln){
+    return pln/4.83;
+}
+int main(){
+
+    int wybor;
+    int kwotaf,kwotapln;
+    cout<<"Program przelicza kwote w DOLARACH na PLN lub odwrotnie.\t"<<endl<<"\t1 - kwota w FUNTACH na PLN"<<endl<<"\t2 - kwota w PLN na FUNTY"<<endl;
+    cout<<"Twoj wybor: "; cin>>wybor;
+
+    if(wybor==1){
+        cout<<endl<<"\tPodaj kwote w PLN:"; cin>>kwotapln;
+        cout<<kwotapln<<" PLN wymienisz na "<<pln_na_funty(kwotapln)<<" FUNTY";
     }
-    else if ((wiek>=18)&&(wiek<35)){
-        cout<<"Jestes pelnoletni i nie mozesz kandydowac";
+    if(wybor==2){
+        cout<<endl<<"\tPodaj kwote w FUNTACH:"; cin>>kwotaf;
+        cout<<kwotaf<<" FUNTY wymienisz na "<<funty_na_pln(kwotaf)<<" PLN";
     }
 
-    else
-    {
-        cout<<"Jestes pelnoletni i mozesz kandydowac na prezydenta";
-    }
     return 0;
 }
